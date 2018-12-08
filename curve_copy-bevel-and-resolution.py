9913f312354e0c5282a241679b88bfb0b0779f5e
RaphaelBeguin <36958850+RaphaelBeguin@users.noreply.github.com>
2018-12-08 13:08:42 +0100

    Update curve_copy-bevel-and-resolution.py

--- curve_copy-bevel-and-resolution.py
+++ curve_copy-bevel-and-resolution.py
@@ -5,5 +5,8 @@ for obj in bpy.context.selected_objects:
     if obj != active_obj:
         # resolution U
         obj.data.resolution_u = active_obj.data.resolution_u 
-        # We could also access other proerties like bevel
+        # added
+        obj.data.extrude = active_obj.data.extrude
         obj.data.bevel_depth = active_obj.data.bevel_depth
+        obj.data.render_resolution_u = active_obj.data.render_resolution_u
+        obj.data.twist_mode = active_obj.data.twist_mode
